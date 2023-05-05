e9a26af7483c1ef3a0bf8af1ba0899c282243f8d
pvigames <89862719+pvigames@users.noreply.github.com>
2023-05-05 09:10:52 -0400

    Update all downed sites

--- javascript/sites.js
+++ javascript/sites.js
@@ -6,15 +6,15 @@ const sites = [
     ["Wordle", "www.nytimes.com/games/wordle/index.html", true], 
     ["Worldle", "worldle.teuteuf.fr/", true], 
     ["WordleU", "www.wordleunlimited.com/", true], 
-    ["Semantle", "semantle.novalis.org/", true],
-    ["Quordle", "www.quordle.com/#/", true], 
-    ["Jsemu2", "jsemu2.github.io/gba/", true], 
+    ["Semantle", "semantle.novalis.org/", false],
+    ["Quordle", "www.quordle.com/#/", false], 
+    ["Jsemu2", "jsemu2.github.io/gba/", false], 
     ["TotallyS", "crabhelper.github.io/CMT/iframe/help.html", false],
-    ["Funblock", "sites.google.com/site/funblocked77/retro-bowl-unblocked", true], 
-    ["BrownCha", "browncha023.github.io/GBA/", true], 
-    ["DriftHunt", "v6p9d9t4.ssl.hwcdn.net/html/1792221/ItchIO/index.html", true],
-    ["TheGameComp", "sites.google.com/site/thegamecompilation/home", true],
-    ["WatchDoc","crabhelper.github.io/CMT/classhelp/science.html", true]
+    ["Funblock", "sites.google.com/site/funblocked77/retro-bowl-unblocked", false], 
+    ["BrownCha", "browncha023.github.io/GBA/", false], 
+    ["DriftHunt", "v6p9d9t4.ssl.hwcdn.net/html/1792221/ItchIO/index.html", false],
+    ["TheGameComp", "sites.google.com/site/thegamecompilation/home", false],
+    ["WatchDoc","crabhelper.github.io/CMT/classhelp/science.html", false]
   ];
 window.addEventListener("load", () => {
     for (let site of sites) {
